5861bca9aa4eb0da913dca9d6b2aad83aafa43fb
rfldxx <166624946+rfldxx@users.noreply.github.com>
2024-04-14 02:06:45 +0300

    Update using_binom.py

--- N'th prefix sum/using_binom.py
+++ N'th prefix sum/using_binom.py
@@ -1,1 +1,18 @@
+def sum_of_nth_iteration(data, N):
+    nc = N
+    kc = 0
+    C  = 1
 
+    ans = 0
+    for i in range(len(data)):
+        ans += C * data[len(data)-1 - i]
+
+        nc += 1
+        kc += 1
+        C   = (nc*C) // kc
+
+    return ans
+
+data = [9, 11, 4, 6, 4, 2]
+ans = sum_of_nth_iteration(data, 7)
+print('\nFOR data: ', data, '\nANS:      ', ans)
