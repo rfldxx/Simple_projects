48ccb627925cbe0451703f2678ea7232b469459a
home_pc <you@example.com>
2024-09-05 00:22:43 +0300

    can move one beld [auto-save]

--- RubikCubeMoves/p_n.cpp
+++ RubikCubeMoves/p_n.cpp
@@ -1,3 +1,6 @@
+// Вращение сторон "кубика-рубика" NxNxN
+// + отображение его "физической" развёртки
+
 #include <iostream>
 #include <vector>
 #include <string>
@@ -14,10 +17,18 @@ public:
 #define DOTS(a, b) , a( N*N, (decltype(a)::value_type)(b) )
     cube(unsigned N) : n(N) FORSIDE(DOTS) {} 
 
+    // Проверка что кубик ещё не собран
     // bool operator !  ()              const { }
+
+    // Проверка двух кубиков на схожесть
     // bool operator != (const cube& b) const { 
         // Если n - нечёт, то проверяем через центры
         // Иначе находим одинаковые углы и далее ...
+    // }
+
+    // Значение на сколько кубик собран
+    // double status() const {
+    //     // Надо наэфективно раставить центры
     // }
 
 
@@ -31,23 +42,26 @@ public:
 #define DECLARE(a, ...) vector<CLR> a;
 FORSIDE(DECLARE)
 
+// ВЫВОД РАЗВЁРТКИ НА ЭКРАН
+// ------------------------
     template <typename Cont>
     stringstream lineout(const Cont& a, int i, int lvl, int add) const;
+    size_t       lineout_size() const { return n*2;  /* n*map_color(0).size(); */ }
+    string       map_color(CLR _) const;
 
-    string map_color(CLR _) const {
-        // decltype(t)::value_type; //::value_type;
-        // ((int)CLR::RED);
-        //     colors[] : [ красный,  оранжевый, зелёный,  синий,     жёлтый,      белый ]
-        string colors[] = {   "101", "48;5;208",   "102",  "104", "48;5;255", "48;5;226" };
-        return string() + "\033[" + (colors[(int)_]) + "m" + "  " + "\033[0m"; 
-    }
 
-    size_t lineout_size() const { return n*2;  /* n*map_color(0).size(); */  }
-
+// ВРАЩЕНИЕ СТОРОН
+// ---------------
+    // вращение "спирали" 
     template <typename Cont>
-    inline void spin( Cont* pos[], int l) {
-        for(int i = 0; i < l; i++)
-            cycle_swap( pos[0][i], pos[1][i], pos[2][i], pos[3][i] );
+    inline void spin(Cont* pos[], int l) {
+        // cursed макрос:  RCY([pos][i])  ->  0[pos][i], ...  <->  pos[0][i]
+        #define RCY(a) 0 a, 1 a, 2 a, 3 a 
+
+        // стартуем с конца участка, так сделано чтобы spin_relaxation отрабатывал эффективнее:
+        //    в таком случае в spin_relaxation "иголка" перебрасывается только для случая c t == 0
+        // for(size_t ii = l; ii != 0; ii--)  
+        while(l--) cycle_swap( RCY([pos][l]) );
     }
 
 
@@ -63,10 +77,34 @@ FORSIDE(DECLARE)
     
     // поворот примыкающих сторон
     template <typename Cont>
-    void spin_relaxation(const Cont* pos[4], const int t[4], int h) {
-        
+    void spin_relaxation(Cont* pos[], const int t[], int h) {
+        for(int i = 0; i < 4; i++) pos[i] += t[i] * (n-1); 
 
-        
+        for(int N = n; h >= 0; N -= 2, h--) {
+            cout << endl << "START AGAIN!!!" << endl;
+            // дебилская t = 3
+            for(int i = 0; i < 4; i++) pos[i] += (N-1) - (t[i]==3)*4*(N-1);
+            spin( pos, h+1 );
+            cout << "spin 1/3 with h = " << h << endl << *this << endl;
+
+            for(int i = 0; i < 4; i++) pos[i] -= (N-1) - (t[i]==3)*4*(N-1); 
+            
+
+            spin( pos, N-1 );
+            cout << "spin 1/2 with h = " << h << endl << *this << endl;
+            cout << endl << "FINISH???" << endl;
+
+            for(int i = 0; i < 4; i++) pos[i] += ( !t[i])*4*(N-1) - h;
+    
+            cout << endl << "FINISH?? !" << endl;
+
+            if(h) spin( pos,  h-1 );
+    
+            cout << endl << "FINISH?!!" << endl;
+
+            for(int i = 0; i < 4; i++) pos[i] += (!!t[i])*4*(N-1) + h;
+            cout << endl << "FINISH!!!" << endl;
+        }
     }
 
 };
@@ -76,38 +114,9 @@ FORSIDE(DECLARE)
 #define inizilization(side, ...) int side[3][3];
 FORSIDE(inizilization)
 
-void init(int m[3][3], int colour) {
-    for(int i = 0; i < 3 ; i++)
-        for(int j = 0; j < 3 ; j++)
-            m[i][j] = colour;
-}
-
-bool chbs(int m[3][3]) {
-    for(int i = 0; i < 3 ; i++) 
-        for(int j = 0; j < 3 ; j++)
-            if(m[i][j] != m[0][0]) return 0;
-    return 1;
-}
-
-bool check() {
-    #define CHBS(side, ...) && chbs(side)
-    return 1 FORSIDE(CHBS);
-}
-
-int& get(int m[3][3], int t, int i, int h = 0) {
-    return *( (int* []) {&m[h][i], &m[i][2-h], &m[2-h][2-i], &m[2-i][h]} [t] );
-}
-
-void spin(int a[3][3], int at,  int b[3][3], int bt,
-          int c[3][3], int ct,  int d[3][3], int dt, int h = 0, int imax = 3) {
-    for(int i = 0; i < imax; i++)
-        cycle_swap( get(a, at, i, h), get(b, bt, i, h), 
-                    get(c, ct, i, h), get(d, dt, i, h) );
-}
-
 
 // Face Move (side)
-#define  INV(...) tINV(__VA_ARGS__)
+#define  INV(...) tINV(__VA_ARGS__) 
 #define tINV(a, at, b, bt, c, ct, d, dt) a,(at+2)%4, d,(dt+2)%4, c,(ct+2)%4, b,(bt+2)%4
 #define SIDE(a) a, 0, a, 1, a, 2, a, 3
 
@@ -118,13 +127,11 @@ void Bname   () { spin(    SIDE(b) , 0, 2); spin(INV(__VA_ARGS__)   ); } \
 void Fname##d() { spin(INV(SIDE(f)), 0, 2); spin(INV(__VA_ARGS__), 2); } \
 void Bname##d() { spin(INV(SIDE(b)), 0, 2); spin(    __VA_ARGS__ , 2); }
 
-INIT_AXIS_MOVE(F, B, fs, bs,  ds, 0, ls, 1, us, 2, rs, 3)
-INIT_AXIS_MOVE(R, L, rs, ls,  bs, 3, ds, 3, fs, 3, us, 3)
-INIT_AXIS_MOVE(U, D, us, ds,  ls, 0, bs, 2, rs, 0, fs, 0)
+// INIT_AXIS_MOVE(F, B, fs, bs,  ds, 0, ls, 1, us, 2, rs, 3)
+// INIT_AXIS_MOVE(R, L, rs, ls,  bs, 3, ds, 3, fs, 3, us, 3)
+// INIT_AXIS_MOVE(U, D, us, ds,  ls, 0, bs, 2, rs, 0, fs, 0)
 
 
-// печать развертки
-void P();
 
 int main() {
     cube x(4);
@@ -133,23 +140,31 @@ int main() {
     x.rs[ 9] = cube::CLR::YELLOW;
     x.rs[13] = cube::CLR::WHITE;    
     x.rs[12] = cube::CLR::ORANGE;
+
+    // x.us[ 5] = cube::CLR::YELLOW;
     cout << "INITIAL CUBE:\n" << x;
     x.spin_face(x.rs);
     cout << "AFTER MOVING rs:\n" << x;
     
+    // x.spin_face(x.rs);
+    // cout << "AFTER MOVING rs:\n" << x;
+
+    // x.spin_face(x.rs);
+    // cout << "AFTER MOVING rs:\n" << x;
+    // x.spin_face(x.rs);
+    // cout << "AFTER MOVING rs:\n" << x;
+
+
+    #define  stp(a, b, c, d) &(a[0]), &(b[0]), &(c[0]), &(d[0])
+    #define  RTA(...) wRTA(__VA_ARGS__)
+    #define wRTA(name, a, ...) decltype(a) name[] = {a, __VA_ARGS__}
+
+    RTA(pp1, stp(x.ds, x.fs, x.us, x.bs));
+    RTA(pp2, 1, 1, 1, 3);
+    x.spin_relaxation(pp1, pp2, 1);
+    cout << "AFTER SPIN BELT rs:\n" << x;
 
     return 0;
-
-#define filling(side, num) init(side, num);
-FORSIDE(filling)
-    
-    fs[0][0] = 1; fs[0][1] = 2; fs[0][2] = 3;
-    fs[1][0] = 4; fs[1][1] = 5; fs[1][2] = 6;
-    fs[2][0] = 7; fs[2][1] = 8; fs[2][2] = 9;
-
-    // P();
-    Fd(); P();
-
 }
 
 
@@ -166,7 +181,7 @@ ostream& operator << (ostream& out, const cube& _) {
 
     // ls | fs | rs | bs
     CRINGEITERATION {
-        for(auto side : ((const decltype(_.ls)* []){&_.ls, &_.fs, &_.rs, &_.bs}))
+        for(auto side : {&_.ls, &_.fs, &_.rs, &_.bs} )
             out << "|" << _.lineout(*side, i, lvl, add).str();
         out << "|\n"; 
     }
@@ -184,7 +199,7 @@ ostream& operator << (ostream& out, const cube& _) {
 }
 
 template <typename Cont>
-stringstream  cube::lineout(const Cont& a, int i, int lvl, int add) const {
+stringstream cube::lineout(const Cont& a, int i, int lvl, int add) const {
     stringstream  out;
     for(int t = 0; t < lvl; t++) out << map_color(a[4*(t+1)*(n-t-1)-i+t]);
 
@@ -195,3 +210,13 @@ stringstream  cube::lineout(const Cont& a, int i, int lvl, int add) const {
     for(int t = lvl; t > 0; t--) out << map_color(a[-4*t*t+4*n*t-3*n+5*t-2+i]);
     return out;
 }
+
+string cube::map_color(CLR _) const {
+    // decltype(t)::value_type; //::value_type;
+    // ((int)CLR::RED);
+    //     colors[] : [ красный,  оранжевый, зелёный,  синий,     жёлтый,      белый ]
+    string colors[] = {   "101", "48;5;208",   "102",  "104", "48;5;255", "48;5;226" };
+    return string() + "\033[" + (colors[(int)_]) + "m" + "  " + "\033[0m"; 
+}
+
+    
