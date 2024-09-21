3cd4b2828fd5d3dc9f8874aae660f4b1ffd757f1
home_pc <you@example.com>
2024-09-22 02:29:24 +0300

    again midnight music

--- TemplateTrain/tuple_analog.cpp
+++ TemplateTrain/tuple_analog.cpp
@@ -48,6 +48,7 @@ struct LAST { };
 
 tmp(T, V) 
 struct collector {
+    using my_type = collector<T, V>;
     T neww;
     V oldd;
 
@@ -61,24 +62,36 @@ struct collector {
     auto push(K a) {
         if constexpr (Npos == 0) return collector<K, my_type> {a, *this};
         else {
-            auto tail = oldd.template push<Npos-1>(a);
+            auto tail = oldd.template push<Npos-1>(a); // чет если претащить это на одну строчку ниже, то компилятор не догадывается об типах аргументов
             return collector<T, decltype(tail)>{neww, tail};
         }
     }
 
-    tmp(size_t Npos, K, L)
-    auto push(collector<K, L> a) {
+
+    // чисто чтобы можно было делать .push_back().push_back().push_back()
+    // в виде fold expression:  ( += ... += args )  [чёт пока мутневатенько]
+    tmp(K)  auto push_back   (K a); 
+    // auto push_back(void a) { return *this; }
+    tmp(K)  auto operator += (K a) { return push_back(a); }
+
+
+    // вставка collector в collector
+    tmp(size_t Npos, K, L) auto push(collector<K, L> a) {
         return this->push<Npos>(a.neww).template push<Npos+1>(a.oldd);
     }
+    tmp(size_t Npos)  auto push(collector<LAST, LAST> a) { return *this; }
 
-    tmp(size_t Npos)
-    auto push(collector<LAST, LAST> a) { return *this; }
 
-    using my_type = collector<T, V>;
-
-    tmp(K, L)
-    friend ostream& operator << (ostream& out, collector<K, L> cltr);
+    tmp(K, L)  friend ostream& operator << (ostream& out, collector<K, L> cltr);
 };
+
+
+template<> tmp(K)
+auto collector<LAST, LAST>::push_back(K a) {  return this->push<0>(a); }
+tmp(T, V) tmp(K)
+auto collector<T, V>::push_back(K a) {
+    return oldd.template push_back(a).template push<0>(neww); 
+}
 
 
 ostream& operator << (ostream& out, collector<LAST, LAST> cltr) {
@@ -91,42 +104,38 @@ ostream& operator << (ostream& out, collector<T, V> cltr) {
 }
 
 
-
-collector<LAST, LAST> make_collector() { return {}; }
-
-tmp(T, x Args) 
-auto make_collector(T a, xAA) {
-    auto tail = make_collector(args...);
-    return collector<T, decltype(tail)>{a, tail};
+tmp(x Args) 
+auto make_collector(xAA) {
+    return (collector<LAST, LAST>{} += ... += args);
 }
 
 
 tmp(T, size_t N, size_t M = 0)
 auto array_to_my_collector(array<T, N>& a) {
-    if constexpr (M == N) return make_collector();
-    else return array_to_my_collector<T, N, M+1>(a).template push<0>(a[M]);
+    if constexpr(M == N) return make_collector();
+    else return make_collector(a[M]) += array_to_my_collector <T, N, M+1>(a);
+// [version 2] else return array_to_my_collector<T, N, M+1>(a).template push<0>(a[M]);
 }
 
 int main() {
-
     // EXAMPLE 1:
-    auto p = make_collector( 1, 4, string{"hi"});
+    auto p = make_collector( 2.4, 4, string{"hi"});
     p.get<1>() *= 2;
     p.get<2>()[0] += 'A'-'a';
     // cout << p.get<1>();
     auto xp = p.push<3>(9).push<0>(10);
     cout << xp;
 
-    // EXAMPLE 2:
+    // // EXAMPLE 2:
     array<string, 3> t{"hi", "mweow", "sad"};
     auto q = array_to_my_collector(t);
     cout << q;
 
-    // EXAMPLE 3:
-    auto r = xp.push<3>(q);
-    cout << r;
+    // // EXAMPLE 3:
+    // auto r = xp.push<3>(q);
+    // cout << r;
 
-    // COMBINE EXAMPLE:
+    // // COMBINE EXAMPLE:
     array<string, 3> tt{"hi", "mweow", "sad"};
     cout << make_collector(2.3).push<0>("START").push<1>(array_to_my_collector(tt));
 }
