f88382afc26341c1bc151c6bf2921c97b4c680b8
home_pc <jimbobra@student.21-school.ru>
2024-11-12 15:09:00 +0300

    some detail to log's for future road

---
+++ TemplateTrain/switch2_draft.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <string>
+#include <stack>
+#include <string.h>
+
+using namespace std;
+
+#define ll long long
+#define llu ll unsigned
+
+
+template<typename IterVal>
+class A {
+public:
+    A(IterVal& aa, int& bb) : a(aa), b(bb) {}
+
+    IterVal& a;
+    int& b;
+};
+
+template<typename Iter>
+class B {
+public:
+    B(Iter pp) : p(pp) { }
+
+    auto val() {
+        return A(*p, counter);
+    }
+
+    auto  get() const { return counter; }
+    auto& get()       { return counter; }
+
+    void operator ++ () {
+        p++;
+        counter++;
+    }
+
+
+    Iter p;
+    
+    int counter = 0;
+};
+
+int main() {
+    vector<int> vv{1, 6, 3, 4, 0, 7, 5};
+    B b(vv.begin());
+
+    while( b.p != vv.end() ) {
+        cout << b.get() << " -> ";
+        {
+            auto c = b.get();
+            c++;
+        }
+        cout << b.get() << " -> ";
+        {
+            auto& c = b.get();
+            c++;
+        }
+        cout << b.get() << "  | ";
+
+        {
+            auto [e, i] = b.val();
+            e++;
+        }
+        
+        auto [e, i] = b.val();
+        
+
+        cout << i << " " << e << endl;
+
+        ++b;
+    }
+
+
+    
+}
