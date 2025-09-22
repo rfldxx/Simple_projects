9e8d42996423baa1fa5a6f00bc215b59263ef3d1
home_pc <jimbobra@student.21-school.ru>
2025-09-23 02:59:33 +0300

    шортсы убивают

--- GeomEdit/alt.cpp
+++ GeomEdit/alt.cpp
@@ -7,7 +7,9 @@ using namespace std;
 #define N 4
 
 
-set<string> history;
+// вообще наверное есть операция "<" на vector<vector<int>>, 
+// но чет не хочется такое сложное использовать
+map<string, vector<vector<int>>> history;
 
 string miku(vector<vector<int>> Matr);
 vector<vector<int>> deobfuscate(vector<vector<int>> stamp);
@@ -23,7 +25,6 @@ vector<vector<int>> save_paint() {
     return deobfuscate(Matr);
 }
 
-bool MIKU = 0;
 
 // красим k-ый прямоугольник
 // возвращает: смогли ли хоть как-то докрасить (хотя всё равно не будем это использовать...)
@@ -35,7 +36,6 @@ bool produce(int k) {
         for(x = 0; x < N; x++)
             if( !paint[y][x] ) goto breakcore;
     // простите за goto
-    if(MIKU) cout << "!!! TERMINATE !!!" << endl;
     return 0;
     breakcore:
 
@@ -44,6 +44,10 @@ bool produce(int k) {
     while( xout < N && !paint[y][xout] ) xout++;
 
     if( k == 1 ) {  // ладно, напишем шорткат
+        // кринж, душат goto
+        vector<vector<int>> vv;
+        string mk;
+
         bool ans = 0;
         for(int i = y; i < N; i++)
             for(int j = x; j < xout; j++) 
@@ -57,13 +61,9 @@ bool produce(int k) {
 
         // это победа, друзья! мы смоги найти какую-то раскраску!
         ans = 1;
-        history.insert( miku(save_paint()) );
-
-        if(MIKU) {
-            cout << "!!! FINAL: " << (ans ? "OK" : "SAD") <<  " !!!" << endl;
-            cout << miku(deobfuscate( save_paint() ));
-            cout << endl << endl;
-        }
+        vv = save_paint();
+        mk = miku(vv);
+        if( !history.count(mk) ) history[mk] = vv;
 
         clearing:
         for(int i = y; i < N; i++)
@@ -73,7 +73,6 @@ bool produce(int k) {
         return ans;
     }
 
-
     // перебор
     bool collect = 0;
     for(int i = y; i < N; i++) {
@@ -82,13 +81,15 @@ bool produce(int k) {
             for(int ii = y; ii <= i; ii++)
                 paint[ii][j] = k;
         
-            if(MIKU) {
-                cout << "COLOR: " << ((char)('A' + (k-1))) << endl;
-                cout << miku(deobfuscate( save_paint() ));
-                cout << " >>> ";
-            }
-        
             collect |= produce(k-1);
+
+            if( k == N ) {
+                cout << "."; fflush(stdout);
+            }
+        }
+
+        if( k == N ) {
+            cout << "Final  y=" << i << "!" << endl;
         }
 
         // возвращаем холст к "началу"
@@ -99,7 +100,6 @@ bool produce(int k) {
 
     return collect;
 }
-
 
 
 vector<vector<int>> deobfuscate(vector<vector<int>> stamp) {
@@ -123,7 +123,6 @@ vector<vector<int>> deobfuscate(vector<vector<int>> stamp) {
         m += xcords[i];
     }
 
-
     vector Matr(n, vector<int>(m));
     int xpos = 0, ypos = 0;
     for(int i = 0; i < N; i++) {
@@ -145,7 +144,7 @@ string miku(vector<vector<int>> Matr) {
     int n = Matr.size(), m = Matr[0].size();
     
     // ВНИМАНИЕ !!!!!
-    if( n > m ) return "[NOT HORIZONTAL]";
+    if( n > m ) return "[NOT HORIZONTAL]\n";
 
     string result;
     for(int i = 0; i < n; i++) {
@@ -157,18 +156,192 @@ string miku(vector<vector<int>> Matr) {
     return result;
 }
 
-// set<string> unique() {
-//     set<string> result;
-//     for(auto e : history)
-//         result.insert( miku(deobfuscate(e)) );
-//     return result;
-// }
+
+
+
+
+// мммм, вот бы имба была если понимать структуру матричного равенства:  A*A = B*B
+// а ещё интересно, умеет ли человечество решать системы уравнений второй степени
+// а ещё, как wolfram аналетически(?) считает:  f(A)
+using   Rect = tuple<int, int, int, int>;
+using paving = pair<vector<Rect>, pair<int, int>>;
+
+paving make_crush(vector<vector<int>> Matr) {
+    int n = Matr.size(), m = Matr[0].size();
+    // assert( n >= m );
+    vector<bool> was(N);
+    vector<Rect> ans(N);
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < m; j++) {
+            int clr = Matr[i][j];
+            if( was[clr-1] ) continue;
+        
+            int yout = i+1, xout = j+1;
+            while( yout < n && Matr[yout][j] == clr ) yout++;
+            while( xout < m && Matr[i][xout] == clr ) xout++;
+            was[clr-1] = 1;
+            ans[clr-1] = {j, xout-1, i, yout-1};
+        }
+    return {ans, {n, m}};
+}
+auto xreflect        (paving components) {
+    auto [n, m] = components.second;
+    for(auto& [xmin, xmax, ymin, ymax] : components.first) {
+        int t = xmin;
+        xmin  = m-1 - xmax;
+        xmax  = m-1 - t; 
+    }
+    return components;
+}
+auto counterclockwise(paving components) {
+    auto [n, m] = components.second;
+    for(auto& [xmin, xmax, ymin, ymax] : components.first) {
+        int t1 = xmin, t2 = xmax;
+        xmin   = ymin;
+        xmax   = ymax;
+        ymin   = m-1 - t2;
+        ymax   = m-1 - t1;
+    }
+    components.second = {m, n};
+    return components;
+}
+
+
+bool equal(paving A, paving B) {
+    auto cA = A.first;
+    auto cB = B.first;
+    for(int i = 0; i < N; i++) {
+        bool find = 0;
+        for(int j = 0; !find && j < N; j++)
+            find = cA[i] == cB[j];
+        if( !find ) return 0;
+        // вот здесь круто было бы по питоновски сделать else после того как цикл полностью завершился:
+        //   for(...)
+        //   else return 0;
+        // а не эти всякие создания новых переменных
+    }
+    return 1;
+}
+
+bool similar(paving A, paving B) {
+    auto [nA, mA] = A.second;
+    auto [nB, mB] = B.second;
+    if( nA != nB || mA != mB ) return 0;
+
+    auto saveA = A;
+
+    // ===================================
+    if( equal(A, B) ) return 1;
+    
+    bool square = nA == mA;
+    A = counterclockwise(A);
+    if( square && equal(A, B) ) return 1;
+
+    A = counterclockwise(A);
+    if( equal(A, B) ) return 1;
+
+    if( square ) {
+        A = counterclockwise(A);
+        if( equal(A, B) ) return 1;
+    }
+    // ===================================
+
+    A = xreflect(saveA);
+    if( equal(A, B) ) return 1;
+    
+    A = counterclockwise(A);
+    if( square && equal(A, B) ) return 1;
+
+    A = counterclockwise(A);
+    if( equal(A, B) ) return 1;
+
+    if( square ) {
+        A = counterclockwise(A);
+        if( equal(A, B) ) return 1;
+    }
+    // ===================================
+    return 0;
+}
+
+
+
+vector<pair< string, paving >> althitory;
+void convert() {
+    for(auto [s, vv] : history)
+        althitory.push_back( {s, make_crush(vv)} );
+}
+
+
+vector<string> alt;
+void unique() {  // сравнивает все пары
+    int k = althitory.size();
+    vector<int> indx;
+    for(int i = 0; i < k; i++) {
+        bool find = 0;
+        for(auto j: indx) {
+            find |= similar( althitory[i].second, althitory[j].second );
+        }
+        if(!find) indx.push_back(i);
+    }
+    for(auto j : indx) alt.push_back(althitory[j].first);
+}
+
+
+
+
+void print(paving A) {
+    auto [ff, ss] = A;
+    printf("RECT:  n=%d x m=%d\n", ss.first, ss.second);
+    for(auto [a, b, c, d] : ff) {
+        printf("  x: [%d %d] , y: [%d %d]\n", a, b, c, d);
+    }
+}
+
+void test_transformation(paving A) {
+    print(A);
+
+    cout << "\nTurn counterclockwise:  90\n";
+    A = counterclockwise(A);
+    print(A);
+
+    cout << "\nTurn counterclockwise: 180\n";
+    A = counterclockwise(A);
+    print(A);
+
+    cout << "\nTurn counterclockwise: 270\n";
+    A = counterclockwise(A);
+    print(A);
+
+
+    A = counterclockwise(A);
+
+    cout << "\nX - reflect\n";
+    A = xreflect(A);
+    print(A);
+
+    cout << "\nTurn counterclockwise:  90 + (x-reflect)\n";
+    A = counterclockwise(A);
+    print(A);
+
+    cout << "\nTurn counterclockwise: 180 + (x-reflect)\n";
+    A = counterclockwise(A);
+    print(A);
+
+    cout << "\nTurn counterclockwise: 270 + (x-reflect)\n";
+    A = counterclockwise(A);
+    print(A);
+}
 
 
 int main() {
     produce(N);
-    cout << history.size() <<  endl;
-    // for(auto s : history) {
-    //     cout << s << endl;
-    // }
+    
+    history.erase( "[NOT HORIZONTAL]\n");
+    convert();
+    unique();
+    
+    cout << history.size() << " / " << alt.size() << endl;
+    for(auto s : alt) {
+        cout << s << endl;
+    }
 }
